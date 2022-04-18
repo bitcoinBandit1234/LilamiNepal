b6e4cbbf084b8a90d531247b01620df237c90133
sandeshshiwakoti <sandeshshiwakoti69@gmail.com>
2022-04-18 14:39:41 +0545

    chat added

--- client/src/component/chatBox/chatBox.jsx
+++ client/src/component/chatBox/chatBox.jsx
@@ -7,22 +7,22 @@ const ChatBox = ({socket, user, seller})=>{
    const [message, setMessage] = useState("");
    const [messageList, setMessageList] = useState([]);
 
-   const sendMessage = ()=>{
+   const sendMessage = async ()=>{
        if(message.trim() != ""){
            const messageInfo = {
                room: seller,
                sender: user.username,
                msg: message
            }
-           socket.emit("sendMessage", messageInfo);
-           setMessageList((list)=>[...list, message])
+           await socket.emit("sendMessage", messageInfo);
+           setMessageList([...messageList, message])
        }
    }
 
 
    useEffect(()=>{
     socket.on('receiveMessage', (data)=>{
-        setMessageList([...messageList, data])
+        setMessageList((list)=>[...list, data])
     })
    }, [socket])
 
@@ -32,10 +32,10 @@ const ChatBox = ({socket, user, seller})=>{
         <h3 className="chatbox__title">Message</h3>
         <div className="chatbox__messages">
             {messageList.map((indieMessage, index)=>{
-                console.log(indieMessage);
-                console.log(user);
+                console.log(messageList);
+                console.log(user.username);
                 return(
-                    <p key={index} className={indieMessage.sender == user.username?"chatbox__messages__sender": "chatbox__messages__sender"}>{indieMessage.msg}</p>
+                    <p key={index} className="chatbox_messages__sender">{indieMessage.msg}</p>
                 );
             })}
         </div>
